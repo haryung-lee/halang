00a854504cf6652b31ac9749481c2e151080c150
leeharyung <lhr4884@naver.com>
2022-12-07 03:36:41 +0900

    chore: delette asePath and assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     loader: "akamai",
     path: "",
   },
-  basePath: "/portfolio",
-  assetPrefix: "/portfolio",
+  // basePath: "/portfolio",
+  // assetPrefix: "/portfolio",
 };
 
 module.exports = nextConfig;
